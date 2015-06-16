1a4abfce822bf8b583d983a9754d948f4623b0c1
zhoukai <zhoukai@nfs.iscas.ac.cn>
2015-06-16 15:09:23 +0800

    remote c/p,string only

--- implements/cpservice.js
+++ implements/cpservice.js
@@ -1,15 +1,51 @@
 var copypaste = require("node-copy-paste"),
     fs = require("fs"),
     os = require("os"),
+    device = require("../../../app/demo-rio/nodewebkit/lib/api/device_service.js"),
+    im = require("../../../framework/api/lib/im.js"),
     remoteProxy = require("../interface/clipboardProxyRemote");
 
 
+var clipContent =undefined,
+		clipstack = undefined;
+
+
 //init the clipstack of a device based on device-discovery service
-function initClipStack(){
+function maintainClipStack(){
+	//init clipstack...
+	if(clipstack === undefined){
+		clipstack = [];
+		device.showDeviceList(function(ddd){
+			console.log("Init clipstack...");
+			for (dev in ddd ) {
+				clipstack.push(ddd[dev].address);
+			}
+		});
+	}
 
+	//update the clipstack when there is a device online or offline.
+	device.addListener(function(_para){
+		if(_para !== 'object') return console.log(_para);
+		var _info = _para.info,
+				_dev_ip = _info.address; 
+		switch(_para.flag){
+			case 'up':
+				if(_dev_ip in clipstack) 
+					break;
+				else clipstack.unshift(_dev_ip);
+				break;
+			case 'down':
+				var id = clipstack.indexOf(_dev_ip);
+				if(id == -1)
+					throw new Error("Warn: this device is not in the clipstack.");
+				else
+					clipstack.splice(id,1);
+				break;
+			default :
+		  	break;
+		}
+	});
 }
-
-var clipContent =undefined;
 
 //this function's fearure is to get the current 
 //content of local system's clipboard
@@ -25,10 +61,12 @@ function getClipboardData(){
 	});
 }
 
-//para @string: data to be copied.
-//para @clipContent: current data of the clipboard
-// if the clipboard is updated,return a mesage to inform other devices;
-//otherwise return false.
+/* para @string: data to be copied.
+*  para @clipContent: current data of the clipboard
+*  if the clipboard is updated,return a mesage to inform other devices;
+*  otherwise return false.
+*  
+*/
 function isClipboardUpdated(string,clipContent,callback){
 	var _string = string;
 	getClipboardData();
@@ -37,25 +75,41 @@ function isClipboardUpdated(string,clipContent,callback){
 		console.log("clipboard's content is not changed.");
 		return false;
 	}else{
-		//broadcast clipboard updated mesage to device in the hoc.
+		//broadcast clipboard updated mesage to device in the AdHoc.
 		var msg = {};
 		msg.ip = os.networkInterfaces().eth0[0].address;
 		console.log("local ip is: " + msg.ip);
 		msg.txt = "content of "+ msg.ip +"'s clipboard is updated!";
 
+		//inform other device in the AdHoc that the content of system clipboard has been updated
+		for (var i = 0; i < clipstack.length; i++) {
+			var _ip = clipstack[i];
+			if(_ip == msg.ip) break;
+			else{
+				var obj = remoteProxy.getRemoteObj('clipboard', _ip);
+				obj.sendmsg(msg,function(res){
+  				console.log(res);
+  				if (res.err) {
+    				console.log(res.err);
+  				}
+				});
+			}
+		};
 		return msg;
 	}
 }
 
-//para @msg:Object,
-//     {ip: string,
-//      txt: string }
-//para @clipstack:Array, 
-//  [ ip:string,
-//    ...
-//  ]
-//storing metadata of each system clipboard
-//in the whole local network
+
+/* para @msg:Object,
+*      {ip: string,
+*       txt: string }
+* para @clipstack:Array, 
+*   [ ip:string,
+*      ...
+*   ]
+* storing metadata of each system clipboard
+* in the whole local network
+*/
 function updateClipStack(msg,clipstack,callback){
 	var _ip = msg.ip;
 	// data structure of the clip stack needed to be initial before doing the following action
@@ -120,7 +174,8 @@ exports.setStub = function(stub_) {
     }else{
     	//remotePaste procedue
     	var _remoteProxy = remoteProxy.getProxy(_ip);
-    	_remoteProxy.paste();
+    	var res = _remoteProxy.paste();
+    	return res;
     }
 	// return clipContent;	
 };
